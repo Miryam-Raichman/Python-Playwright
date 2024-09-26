97bed8d5028064938c760bed351ec80000f3519b
Miryam Raichman <m0556743338@gmail.com>
2024-09-26 12:38:38 +0300

    Add home work

---
+++ Home Work.py
@@ -0,0 +1,16 @@
+#exercise 5
+x=int(input("input number"))
+print(x%2==0)
+#homework
+#exercise 1
+x1,x2=input("input 2 numbers").split()
+print(int(x2)-int(x1))
+#exercise 2
+X=int(input("input mishkal"))
+print(int(X/1000))
+#exercise 3
+x1,x2=input("input 2 numbers").split()
+print(float(x1+x2)%7==0)
+#exercise 4
+x=int(input("input number"))
+print(x%10)
--- first page.py
+++
@@ -1,3 +0,0 @@
-x=int(input("input number"))
-y=int(input("input number"))
-print(int((2*x)-y+(x/3)))
